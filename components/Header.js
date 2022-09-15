b3d761cfa4beabf3c71535fcf2cb52f578892fe5
cheeze2000 <me@cheeze2000.net>
2022-09-15 23:44:13 +0800

    feat: header

---
+++ components/Header.js
@@ -0,0 +1,20 @@
+import Image from "next/image";
+
+function Header() {
+    return (
+        <div className="flex justify-evenly">
+            <div className="relative w-32 aspect-square">
+                <Image
+                    src="/logo.png"
+                    alt="Cat avatar generator logo"
+                    layout="fill"
+                />
+            </div>
+            <div className="flex items-center px-3 font-nunito text-3xl sm:text-4xl text-center text-pcorange">
+                Cat Avatar Generator
+            </div>
+        </div>
+    );
+}
+
+export default Header;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
+import Header from "~/components/Header";
+
 function Home() {
     return (
-        <>
-        </>
+        <div className="max-w-screen-sm min-h-screen p-6 mx-auto bg-white">
+            <Header />
+        </div>
     );
 }
 
