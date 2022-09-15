ac8cd136d19832738d02abadbaa0e2fe7dce8c7f
cheeze2000 <me@cheeze2000.net>
2022-09-16 00:50:01 +0800

    feat: add author name

--- components/Header.js
+++ components/Header.js
@@ -2,16 +2,27 @@ import Image from "next/image";
 
 function Header() {
     return (
-        <div className="flex justify-evenly">
-            <div className="relative w-32 aspect-square">
+        <div className="flex flex-col sm:flex-row justify-evenly items-center gap-3 sm:gap-0">
+            <div className="relative w-24 sm:w-32 aspect-square">
                 <Image
                     src="/logo.png"
                     alt="Cat avatar generator logo"
                     layout="fill"
                 />
             </div>
-            <div className="flex items-center px-3 font-nunito font-bold text-3xl sm:text-4xl text-center text-pcorange">
-                Cat Avatar Generator
+            <div className="flex flex-col justify-center font-nunito">
+                <div className="font-bold text-2xl sm:text-4xl text-center text-pcorange">
+                    Cat Avatar Generator
+                </div>
+                <div className="text-center text-sm sm:text-base">
+                    <span>by </span>
+                    <a
+                        className="hover:underline underline-offset-2"
+                        href="https://www.davidrevoy.com/"
+                    >
+                        David Revoy
+                    </a>
+                </div>
             </div>
         </div>
     );
