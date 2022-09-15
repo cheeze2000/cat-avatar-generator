77696746bedb8f1e6db1479bd280f89bd0cd2932
cheeze2000 <me@cheeze2000.net>
2022-09-15 22:34:22 +0800

    refactor: reset template files

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,6 @@
-import Counter from "~/components/Counter";
-
-export async function getServerSideProps() {
-    const res = await fetch("https://catfact.ninja/fact");
-    return { props: await res.json() };
-}
-
-function Home(props) {
+function Home() {
     return (
         <>
-            <div className="flex justify-center items-center w-screen h-screen">
-                <Counter />
-            </div>
-            <div className="fixed bottom-0 left-0">
-                <p>Random cat fact: {props.fact}</p>
-            </div>
         </>
     );
 }
