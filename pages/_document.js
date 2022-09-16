a9d37cb1964fd0261ab8b82c90216196dfa575a3
cheeze2000 <me@cheeze2000.net>
2022-09-16 20:00:26 +0800

    feat: seo tags

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,12 @@ export default function Document() {
     return (
         <Html lang="en">
             <Head>
+                <meta name="description" content="A free and open-source API to generate cat avatars" />
+                <meta name="theme-color" content="#A5C2E1" />
+                <meta property="og:description" content="A free and open-source API to generate cat avatars" />
+                <meta property="og:title" content="Cat Avatar Generator" />
+                <meta property="og:type" content="website" />
+                <meta property="og:url" content="https://cat-avatars.vercel.app" />
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
                 <link href="https://fonts.googleapis.com/css2?family=Inconsolata&family=Nunito:wght@400;700&family=Source+Sans+Pro:wght@400;700&display=swap" rel="stylesheet" />
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import Head from "next/head";
+
 import About from "~/components/About";
 import Demo from "~/components/Demo";
 import Header from "~/components/Header";
@@ -5,12 +7,17 @@ import Usage from "~/components/Usage";
 
 function Home() {
     return (
-        <div className="flex flex-col gap-12 max-w-screen-sm min-h-screen p-6 mx-auto bg-white">
-            <Header />
-            <Demo />
-            <Usage />
-            <About />
-        </div>
+        <>
+            <Head>
+                <title>Cat Avatar Generator</title>
+            </Head>
+            <div className="flex flex-col gap-12 max-w-screen-sm min-h-screen p-6 mx-auto bg-white">
+                <Header />
+                <Demo />
+                <Usage />
+                <About />
+            </div>
+        </>
     );
 }
 
