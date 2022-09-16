f780562f01956f4b699a0dc88301b3123443e4be
cheeze2000 <me@cheeze2000.net>
2022-09-16 16:11:24 +0800

    refactor: make text bolder

--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,7 @@ export default function Document() {
             <Head>
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-                <link href="https://fonts.googleapis.com/css2?family=Inconsolata&family=Nunito:wght@600&family=Source+Sans+Pro:wght@400;700&display=swap" rel="stylesheet" />
+                <link href="https://fonts.googleapis.com/css2?family=Inconsolata&family=Nunito:wght@400;700&family=Source+Sans+Pro:wght@400;700&display=swap" rel="stylesheet" />
             </Head>
             <body>
                 <Main />
