34862ae4c8dbe2e90baaf9bbbf0f6c6d000782f5
cheeze2000 <me@cheeze2000.net>
2022-09-16 08:20:20 +0800

    refactor: next.js pedantry

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,7 @@ export default function Document() {
         <Html lang="en">
             <Head>
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
                 <link href="https://fonts.googleapis.com/css2?family=Inconsolata&family=Nunito:wght@600&family=Source+Sans+Pro:wght@400;700&display=swap" rel="stylesheet" />
             </Head>
             <body>
