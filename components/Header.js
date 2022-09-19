e67e3fe86250ea79f53bd09a82f35b02eb78ec42
cheeze2000 <me@cheeze2000.net>
2022-09-19 23:09:52 +0800

    feat: hyperlink cat logo to homepage

--- components/Header.js
+++ components/Header.js
@@ -1,16 +1,19 @@
 import Image from "next/image";
+import Link from "next/link";
 
 function Header() {
     return (
         <div className="flex flex-col sm:flex-row justify-evenly items-center gap-3 sm:gap-0">
-            <div className="relative w-24 sm:w-32 aspect-square">
-                <Image
-                    src="/logo.png"
-                    alt="Cat avatar generator logo"
-                    layout="fill"
-                    priority="true"
-                />
-            </div>
+            <Link href="/">
+                <a className="relative w-24 sm:w-32 aspect-square">
+                    <Image
+                        src="/logo.png"
+                        alt="Cat avatar generator logo"
+                        layout="fill"
+                        priority="true"
+                    />
+                </a>
+            </Link>
             <div className="flex flex-col justify-center font-nunito">
                 <div className="font-bold text-2xl sm:text-4xl text-center text-pcorange">
                     Cat Avatar Generator
